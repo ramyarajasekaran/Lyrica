be05f6065d0a35b5e8eea749ee850593bf5c16f9
ramyarajasekaran <Ramya@Ramyas-MacBook-Pro.local>
2017-09-22 00:00:31 -0500

    added api.ai stuff -> now lyricabot does smalltalk

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-const apiaiApp = require('apiai')(e552a3546d914402ba3c18b5f6145d5a);
+const apiaiApp = require('apiai')(process.env.CLIENT_ACCESS_TOKEN);
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -35,6 +35,7 @@ app.post('/webhook/', function (req, res) {
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
+		
 		if (event.message && event.message.text) {
 			let text = event.message.text
 			if (text === 'Generic'){ 
@@ -42,56 +43,74 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			sendMessage(event)
+			sendMessage(sender,text)
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			//sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
 			continue
 		}
 	}
 	res.sendStatus(200)
 })
 
+app.post('/ai', (req, res) => {
+  //console.log('*** Webhook for api.ai query ***');
+  //console.log(req.body.result);
+
+  if (req.body.result.action === 'weather') {
+ //  console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+process.env.WEATHER_API_KEY+'&q='+city;
+
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
+       // console.log(json);
+        let tempF = ~~(json.main.temp * 9/5 - 459.67);
+        let tempC = ~~(json.main.temp - 273.15);
+        let msg = 'The current condition in ' + json.name + ' is ' + json.weather[0].description + ' and the temperature is ' + tempF + ' ℉ (' +tempC+ ' ℃).'
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'
+        });
+      } else {
+        let errorMessage = 'I failed to look up the city name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    })
+  }
+
+});
+
 
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.FB_PAGE_ACCESS_TOKEN
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
+function sendMessage(sender,text) {
+ // let sender = event.sender.id;
+ // let text = event.message.text;
 
   let apiai = apiaiApp.textRequest(text, {
     sessionId: 'tabby_cat' // use any arbitrary id
   });
 
- apiai.on('response', (response) => {
-  let aiText = response.result.fulfillment.speech;
+  apiai.on('response', (response) => {
+    // Got a response from api.ai. Let's POST to Facebook Messenger
+	//console.log(response);
 
-    request({
+    let aiText = response.result.fulfillment.speech;
+
+    request({ 
       url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+      qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
       method: 'POST',
       json: {
         recipient: {id: sender},
@@ -104,7 +123,7 @@ function sendMessage(event) {
           console.log('Error: ', response.body.error);
       }
     });
- });
+  });
 
   apiai.on('error', (error) => {
     console.log(error);
@@ -112,6 +131,7 @@ function sendMessage(event) {
 
   apiai.end();
 }
+
 
 function sendGenericMessage(sender) {
 	let messageData = {
